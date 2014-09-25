0f75427c602a724bf7867109d940e16584cb53cc
Nicholas Lee <csudcy@gmail.com>
2014-09-26 00:42:52 +0100

    Added will

---
+++ plugins/hello.py
@@ -0,0 +1,9 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+
+class HelloPlugin(WillPlugin):
+
+    @respond_to("^hello")
+    def hello(self, message):
+        self.reply(message, "hi!")
---
+++ run_will.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from will.main import WillBot
+
+if __name__ == '__main__':
+    bot = WillBot()
+    bot.bootstrap()
