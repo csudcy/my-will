c2f26cfd15c5842d68462fc8d2abb47534787816
Nicholas Lee <csudcy@gmail.com>
2014-09-26 02:07:51 +0100

    Trying to connect to hipchat...

--- run_will.py
+++ run_will.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 from will.main import WillBot
 
+
+"""
+The below is copied from https://github.com/skoczen/will/blob/master/will/main.py
+It seems that sleekxmpp resolves URLs to IPs if no address is provided to connect
+( see https://github.com/fritzy/SleekXMPP/blob/master/sleekxmpp/clientxmpp.py#L149 )
+Therefore, we'll provide an address!
+"""
+class MyWillBot(WillBot):
+    def connect(*args, **kwargs):
+        if 'address' not in kwargs:
+            kwargs['address'] = (settings.HIPCHAT_SERVER, 5222)
+        WillBot.connect(self, *args, **kwargs)
+
+
 if __name__ == '__main__':
-    bot = WillBot()
+    bot = MyWillBot()
     bot.bootstrap()
