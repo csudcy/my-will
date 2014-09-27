4be73d0b9d48ae3b016cfc57ba25d5ed24441f5a
Nicholas Lee <csudcy@gmail.com>
2014-09-27 19:22:16 +0100

    Debugging emoticons plugin

--- plugins/emoticons.py
+++ plugins/emoticons.py
@@ -8,9 +8,12 @@ import traceback
 from memoize import Memoizer
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+import requests
 
 
 logger = logging.getLogger(__name__)
+
+logger.critical('load start')
 
 EMOTICONS_URL = "https://%(server)s/v2/emoticon?max-results=1000&auth_token=%(token)s"
 
@@ -36,6 +39,7 @@ class HipchatEmoticonsMixin(object):
         """
         Fetch the list of emoticons from hipchat
         """
+        logger.critical('get_emoticon_list start')
         url = EMOTICONS_URL % {
             "server": settings.HIPCHAT_SERVER,
             "token": settings.V2_TOKEN
@@ -43,6 +47,7 @@ class HipchatEmoticonsMixin(object):
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
         response = requests.get(url, headers=headers, data=json.dumps(data))
         data = response.read()
+        logger.critical('get_emoticon_list end')
         return json.loads(data)
 
     @error_logger
@@ -50,12 +55,14 @@ class HipchatEmoticonsMixin(object):
         """
         Find emoticons based on the given search string
         """
+        logger.critical('find_emoticons start')
         emoticons = self.get_emoticon_list()
         if search:
             emoticons = filter(
                 emoticons,
                 lambda e: re.match(search, e['shortcut'])
             )
+        logger.critical('find_emoticons end')
         return map(
             emoticons,
             lambda e: '(%s)' % e['shortcut']
@@ -68,18 +75,24 @@ class EmoticonPlugin(WillPlugin, HipchatEmoticonsMixin):
     @error_logger
     def single(self, message, search=None):
         "emoticon me ___: Search hipchat emoticons for ___ and return a random one"
+        logger.critical('single start')
         emoticons = self.find_emoticons(search)
         if emoticons:
             self.reply(message, random.choice(emoticons))
         else:
             self.reply(message, 'I cannae find any captain!')
+        logger.critical('single end')
 
     @respond_to("^emoticons me (?P<search>.*?)")
     @error_logger
     def list(self, message, search=None):
         "emoticons me ___: Search hipchat emoticons for ___ and return all of them"
+        logger.critical('list start')
         emoticons = self.find_emoticons(search)
         if emoticons:
             self.reply(message, json.dumps(emoticons))
         else:
             self.reply(message, 'I cannae find any captain!')
+        logger.critical('list end')
+
+logger.critical('load end')
