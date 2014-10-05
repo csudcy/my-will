9555d32cfe0e84761c115ae77d36937d2024d7ba
Nicholas Lee <csudcy@gmail.com>
2014-10-06 00:53:48 +0100

    Refactor hangman & fix

--- plugins/hangman.py
+++ plugins/hangman.py
@@ -118,7 +118,7 @@ HANGMAN_STATES = [
 ]
 
 
-REVEAL_TEMPLATE = """
+SECRETS_TEMPLATE = """
     word_count    : {word_count}
     state         : {state}
     word          : {word}
@@ -144,8 +144,8 @@ STATUS_TEMPLATE = """
     Wrong guesses: {guesses_wrong}
 """
 
-class HangmanPlugin(WillPlugin):
-    def __init__(self, *args, **kwargs):
+class HangmanMixin(object):
+    def __init__(self):
         # Load the dictionary
         with open('/usr/share/dict/words', 'r') as f:
             contents = f.read()
@@ -159,13 +159,12 @@ class HangmanPlugin(WillPlugin):
         self.guesses_right = None
         self.guesses_wrong = None
 
-    @respond_to("^hangman me$")
-    def hangman_me(self, message):
+    def new_game(self):
         """
         Start a new game of hangman
         """
         if self.state == 'PLAYING':
-            return self.reply(message, 'You have to finish the current game first!')
+            return 'You have to finish the current game first!'
 
         # Setup a new game
         self.state = 'PLAYING'
@@ -174,19 +173,15 @@ class HangmanPlugin(WillPlugin):
         self.guesses_wrong = []
 
         # Show the user what's going on
-        self.output_status(message)
+        return self.get_status()
 
-    @respond_to("^hangman status$")
-    def hangman_status(self, message):
+    def get_status(self):
         """
-        Check the progress of the current game
+        Get the text represenation of the current game state
         """
-        self.output_status(message)
-
-    def output_status(self, message):
         # Check we're currently playing
         if self.state == 'NOT_PLAYING':
-            return self.reply(message, 'Doesn\'t look like you\'ve started a game yet?')
+            return 'Sorry, it doesn\'t look like you\'ve started a game yet?'
 
         # Prepare the output
         output = STATUS_TEMPLATE.format(
@@ -203,40 +198,87 @@ class HangmanPlugin(WillPlugin):
             output += '\n GAME OVER! \n'
 
         # Send back the message
-        self.reply(message, output)
+        return output
 
-    @respond_to("^hangman reveal$")
-    def hangman_reveal(self, message):
-        # Reveal hangmans inner secrets
-        self.reply(message, 'Here is my current state:')
-        self.reply(message, REVEAL_TEMPLATE.format(
+    def get_secrets(self):
+        """
+        Reveal all my inner secrets...
+        """
+        return SECRETS_TEMPLATE.format(
             word_count=len(self.words),
             state=self.state,
             word=self.word,
             word_revealed=self.word_revealed,
             guesses_right=self.guesses_right,
             guesses_wrong=self.guesses_wrong,
-        ))
+        )
+
+    def guess(self, guess):
+        """
+        Make a guess
+        """
+        # Check we're currently playing
+        if self.state != 'PLAYING':
+            return 'Sorry, it doesn\'t look like you\'re currently playing a game?'
+
+        # Check the input
+        guess = guess.strip().upper()
+        if not re.match('^[A-Z]$'):
+            return 'That\'s not a letter!'
+
+        # Check it hasnt been tried before
+        if guess in self.guesses_right or guess in self.guesses_wrong:
+            return 'You already tried {guess}!'.format(
+                guess=guess,
+            )
+
+        if guess in self.word:
+            self.guesses_right.append(guess)
+
+            # Update word revealed
+            self.word_revealed = ' '.join([w if w in self.guesses_right else '_' for w in self.word])
+
+            # Check if they won
+            if not '_' in self.word_revealed:
+                self.state = 'WON'
+        else:
+            self.guesses_wrong.append(guess)
+
+            # Check if they lost
+            if len(self.guesses_wrong) == len(HANGMAN_STATES):
+                self.state = 'LOST'
+
+        return self.get_status()
+
+
+class HangmanPlugin(WillPlugin, HangmanMixin):
+    def __init__(self, *args, **kwargs):
+        HangmanMixin.__init__(self)
+        WillPlugin.__init__(self, *args, **kwargs)
+
+    @respond_to("^hangman me$")
+    def hangman_me(self, message):
+        """
+        Start a new game of hangman
+        """
+        self.reply(message, self.new_game())
+
+    @respond_to("^hangman status$")
+    def hangman_status(self, message):
+        """
+        Check the progress of the current game
+        """
+        self.reply(message, self.get_status())
+
+    @respond_to("^hangman reveal$")
+    def hangman_reveal(self, message):
+        # Reveal hangmans inner secrets
+        self.reply(message, 'Here are all my secrets:')
+        self.reply(message, self.get_secrets())
 
     @respond_to("^hangman guess (?P<guess>.)$")
     def hangman_guess(self, message, guess):
         """
         Make a guess in the current hangman game
         """
-        # Prepare the input
-        guess = guess.strip().upper()
-
-        # Check it hasnt been tried before
-        if guess in guesses_right or guess in guesses_wrong:
-            return self.reply(message, 'You already tried {guess}!'.format(
-                guess=guess,
-            ))
-
-        if guess in self.word:
-            guesses_right.append(guess)
-            # Update word revealed
-            self.word_revealed = ' '.join([w else '_' for w in self.word if w in self.guesses_right])
-        else:
-            guesses_wrong.append(guess)
-
-        self.output_status(message)
+        return self.reply(message, self.guess(guess))
