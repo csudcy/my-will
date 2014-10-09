2c29434c765fbbdaa1d65eefbd2df4a78bb0847e
Toby Petty <tobypetty1@gmail.com>
2014-10-09 13:08:38 +0100

    fixed lol

--- plugins/lol.py
+++ plugins/lol.py
@@ -1,13 +1,7 @@
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 
-from helpers.dictionary import Dictionary
-
-
 class LOLPlugin(WillPlugin):
-    def __init__(self, *args, **kwargs):
-        self.dictionary = Dictionary()
-        return WillPlugin.__init__(self, *args, **kwargs)
 
     @randomly(num_times_per_day=360*24)
     def define(self, message, word):
