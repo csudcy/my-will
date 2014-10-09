31c9c0011fa09a8e84268951e34333ff0ccda5e4
Danny Butler <danny.butler@artsalliancemedia.com>
2014-10-09 15:28:10 +0100

    Adding more buzzwords

--- helpers/business.py
+++ helpers/business.py
@@ -4,7 +4,7 @@ import random
 def acquire_business():
     buzzwords_file = open('buzzwords.json')
     buzzwords = json.load(buzzwords_file)
-    business = random.choice(buzzwords['before']) + ' ' + random.choice(buzzwords['nouns']) + ' '  + random.choice(buzzwords['after'])
+    business = random.choice(buzzwords['before']) + ' ' + random.choice(buzzwords['before']) + ' ' + random.choice(buzzwords['nouns']) + ' '  + random.choice(buzzwords['after'])
     return business
 
 if __name__ == '__main__':
