ae199031e9d7cd7b76034c86d61d5a9cb7549b4f
Toby Petty <tobypetty1@gmail.com>
2014-10-09 11:48:19 +0100

    my will

---
+++ plugins/lol.py
@@ -0,0 +1,17 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+from helpers.dictionary import Dictionary
+
+
+class LOLPlugin(WillPlugin):
+    def __init__(self, *args, **kwargs):
+        self.dictionary = Dictionary()
+        return WillPlugin.__init__(self, *args, **kwargs)
+
+    @randomly(num_times_per_day=3600*24)
+    def define(self, message, word):
+        """
+        define ___: Get the definition of a word
+        """
+        return self.reply(message, 'LOL')
