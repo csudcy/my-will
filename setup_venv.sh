ad5b07cfdfd1367ff1335a88a872c5986267ed63
Toby Petty <tobypetty1@gmail.com>
2014-10-09 15:24:12 +0100

    postactivate

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 pip install virtualenv
+pip install virtualenvwrapper
 
 virtualenv --no-site-packages ./venv
 
+cp ./postactivate ./venv/bin/postactivate
 chmod +x ./venv/bin/activate
 source ./venv/bin/activate
 
