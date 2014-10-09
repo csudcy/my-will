4869f9c1dcd0f335151fa122f46846d14ea90d09
Danny Butler <danny.butler@artsalliancemedia.com>
2014-10-09 14:35:41 +0100

    Small changes to test plugin

--- plugins/blarg.py
+++ plugins/blarg.py
@@ -1,6 +1,5 @@
 from will.plugin import WillPlugin
-from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
-
+from will.decorators import respond_to
 
 class BlargPlugin(WillPlugin):
     def __init__(self, *args, **kwargs):
@@ -9,6 +8,6 @@ class BlargPlugin(WillPlugin):
     @respond_to("^blarg")
     def define(self, message, word):
         """
-        define ___: Get the definition of a word
+        blarg: Say hi!
         """
         return self.reply(message, 'Hi Danny!')
