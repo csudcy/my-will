1682e5612061099450f7feaeafea64b41edafc7e
Fran Smith <fran.smith@artsalliancemedia.com>
2014-10-09 15:14:38 +0100

    fixes

--- plugins/tfl.py
+++ plugins/tfl.py
@@ -1,6 +1,3 @@
-import json
-import random
-
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
 
@@ -10,14 +7,14 @@ from helpers.tfl import TFL
 class TFLPlugin(WillPlugin):
 
     def __init__(self, *args, **kwargs):
-        self.emoticons = TFL()
+        self.tfl = TFL()
         return WillPlugin.__init__(self, *args, **kwargs)
 
 
-    @respond_to("^get line status$")
-    def line_status(self, message):
+    @respond_to("^tfl me$")
+    def tfl_me(self, message):
         """
-        line status: Get the status of the current line
+        tfl_me: Get the status of the current line
         """
         line_status = self.tfl.get_line_status(message)
         self.say(line_status)
