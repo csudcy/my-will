35622294fa2adb17af212a2b8dea96ad6853b262
Leo Hemsted <leohemsted@gmail.com>
2014-11-13 18:16:23 +0000

    multiple words
    yeah

--- helpers/urban_dictionary.py
+++ helpers/urban_dictionary.py
@@ -5,7 +5,7 @@ import requests, urllib
 class UrbanDictionary(object):
     def get_definition(self, word):
         word = urllib.quote(word.lower())
-        req = requests.get('http://urbanscraper.herokuapp.com/define/%s' % word)
+        req = requests.get('http://urbanscraper.herokuapp.com/define/%s' % word.strip().replace(' ', '+')
         defn = req.json()
         if 'message' in defn:
             return "That's not slang fool!"
@@ -15,3 +15,4 @@ if __name__ == '__main__':
     dm = UrbanDictionary()
     print dm.get_definition('janky')
     print dm.get_definition('asdfasdfzzz')
+    print dm.get_definition('food press')
