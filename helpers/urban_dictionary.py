ba615ad57fd6a5f4849fbf9bf87fc3be3f327655
Leo Hemsted <leohemsted@gmail.com>
2014-11-13 18:25:22 +0000

    it works now

--- helpers/urban_dictionary.py
+++ helpers/urban_dictionary.py
@@ -4,12 +4,13 @@ import requests, urllib
 
 class UrbanDictionary(object):
     def get_definition(self, word):
-        word = urllib.quote(word.lower())
-        req = requests.get('http://urbanscraper.herokuapp.com/define/%s' % word.strip().replace(' ', '+')
+        req = requests.get('http://urbanscraper.herokuapp.com/define/%s' % word.lower().strip().replace(' ', '+'))
         defn = req.json()
         if 'message' in defn:
+            if 'Something went wrong along the way.' in defn['message']:
+                return "idk urbanscraper.herokuapp is broken or some shit"
             return "That's not slang fool!"
-        return "%s: %s\nUsage: %s" % (defn['term'], defn['definition'], defn['example'])
+        return "%s: %s\nUsage: %s" % (word.title(), defn['definition'], defn['example'])
 
 if __name__ == '__main__':
     dm = UrbanDictionary()
